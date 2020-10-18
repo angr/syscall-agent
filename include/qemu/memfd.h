38d1b784f124ee11757f57fe182d710d8761007d
Audrey Dutcher <audrey@rhelmot.io>
2020-10-18 15:03:50 -0700

    Fix memfd_create stub

--- include/qemu/memfd.h
+++ include/qemu/memfd.h
@@ -1,6 +1,7 @@
-#include "qemu/osdep.h"
+#include <sys/syscall.h>
 
-#if defined CONFIG_LINUX && !defined CONFIG_MEMFD
+// hack: use the presence of the memfd syscall as a proxy for the presence of the glibc wrapper
+#ifndef __NR_memfd_create
 inline int memfd_create(const char *name, unsigned int flags) {
 	errno = ENOSYS;
 	return -1;
